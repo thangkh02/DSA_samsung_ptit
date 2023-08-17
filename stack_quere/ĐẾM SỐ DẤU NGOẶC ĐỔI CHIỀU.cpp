239ade858b28ef93d3b3e4e4abb2590f7b0483a6
thangkh02 <138497084+thangkh02@users.noreply.github.com>
2023-08-17 08:40:14 +0700

    Update and rename dsa7004.cpp to ĐẾM SỐ DẤU NGOẶC ĐỔI CHIỀU.cpp

--- stack_quere/ĐẾM SỐ DẤU NGOẶC ĐỔI CHIỀU.cpp
+++ stack_quere/ĐẾM SỐ DẤU NGOẶC ĐỔI CHIỀU.cpp
@@ -1,3 +1,14 @@
+/*Cho một xâu chỉ gồm các kí tự ‘(‘, ‘) và có độ dài chẵn. Hãy đếm số lượng dấu ngoặc cần phải đổi chiều ít nhất, sao cho xâu mới thu được là một dãy ngoặc đúng.
+
+Input:
+
+Dòng đầu tiên là số lượng bộ test T (T ≤ 20).
+
+Mỗi test gồm 1 xâu S có độ dài không vượt quá 100 000, chỉ gồm dấu ( và ).
+
+Output: 
+
+Với mỗi test, in ra đáp án tìm được trên một dòng.*/
 #include <bits/stdc++.h>
 using namespace std;
 void solve(){
