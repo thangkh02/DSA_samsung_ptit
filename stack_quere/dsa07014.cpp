5780dc6238eae6538d1ecb395b426c0ace0e6a7d
thangkh02 <138497084+thangkh02@users.noreply.github.com>
2023-08-17 08:35:33 +0700

    Update dsa07014.cpp

--- stack_quere/dsa07014.cpp
+++ stack_quere/dsa07014.cpp
@@ -1,1 +1,35 @@
+/* tính giá trị biểu thức tiền tố  */
+#include<bits/stdc++.h>
 
+using namespace std;
+
+#define ll long long
+int calc(int x, int y,char d){
+    if(d == '+') return x + y;
+    if(d == '-') return x - y;
+    if(d == '*') return x * y;
+    return x / y;
+}
+int solve(string s){
+    stack<int> st;
+    for(int i=s.size()-1;i>=0;i--){
+        if(isdigit(s[i])) st.push(s[i]-'0');
+        else{
+            int x1 = st.top();st.pop();
+            int x2 = st.top();st.pop();
+            st.push(calc(x1,x2,s[i]));
+        }
+    }
+    return st.top();
+}
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        string s;
+        cin>>s;
+        cout<<solve(s)<<endl;
+    }
+    return 0;
+    
+}
